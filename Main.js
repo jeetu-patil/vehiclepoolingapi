cbed6eb92db54dea7498fc654a601e3a3df22a9a
Parthpatodi <patodiparth@gmail.com>
2022-05-04 18:43:34 +0530

    parth user verification

--- Main.js
+++ Main.js
@@ -3,6 +3,7 @@ const cors=require('cors');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 const app = express();
+const path = require("path");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,"public")));
@@ -13,6 +14,7 @@ const placeRouter=require("./router/PlaceRouter");
 const userRouter = require("./router/UserRouter");
 const publishRideRouter=require("./router/PublishRideRouter");
 const bookRideRouter = require("./router/BookRideRouter");
+app.set("view engine","ejs");
 
 mongoose.connect("mongodb+srv://jitu:jitu%40123@cluster0.5msi4.mongodb.net/carpooling?retryWrites=true&w=majority")
 .then(()=>{
