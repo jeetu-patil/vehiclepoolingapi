aaf9d6147659217977540cc84ffc640e0345d29a
jeetu-patil <jitupatil961@gmail.com>
2022-05-05 10:03:27 +0530

    jeetu-publish

--- Main.js
+++ Main.js
@@ -2,7 +2,6 @@ const express=require('express');
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
-const path=require('path');
 const app = express();
 const path = require("path");
 app.use(cors());
