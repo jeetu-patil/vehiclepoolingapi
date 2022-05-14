16e07aae7f0e702236eb1841b148de13c5134738
jeetu-patil <jitupatil961@gmail.com>
2022-05-14 19:49:58 +0530

    jeetu

--- router/PublishRideHistoryRouter.js
+++ router/PublishRideHistoryRouter.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const publishRideController = require("../controller/PublishRideHistoryController");
 
 router.get("/viewpublisherhistory/:publisherId", publishRideController.viewPublisherHistory);
-
+router.get("/publishhistiry/:publisherId", publishRideController.publishHistory);
 
 module.exports =router;
